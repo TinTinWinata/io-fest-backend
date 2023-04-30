dfdf05998ce25218ce681f689c6652993a0e3eb3
TinTinWinata <tintin6892@gmail.com>
2023-04-30 17:12:21 +0700

    feat: add see for admin, doctor, and member

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,23 +3,49 @@ import { hash } from 'bcrypt';
 const prisma = new PrismaClient();
 async function main() {
   const saltRounds = 10;
-  const hashedPassword = await hash('test', saltRounds);
 
   const testUser = await prisma.user.upsert({
     where: { email: 'test@gmail.com' },
     update: {},
     create: {
-      email: 'test@gmail.com',
-      name: 'Test',
-      password: hashedPassword,
+      email: 'member@gmail.com',
+      name: 'Member',
+      password: await hash('member', saltRounds),
       username: 'Test',
       profilePicture: '/profile.webp',
       isActive: true,
+      role: 'Member',
+    },
+  });
+  const testUser1 = await prisma.user.upsert({
+    where: { email: 'doctor@gmail.com' },
+    update: {},
+    create: {
+      email: 'doctor@gmail.com',
+      name: 'Doctor',
+      password: await hash('doctor', saltRounds),
+      username: 'Doctor',
+      profilePicture: '/profile.webp',
+      isActive: true,
+      role: 'Doctor',
+    },
+  });
+  const testUser2 = await prisma.user.upsert({
+    where: { email: 'admin@gmail.com' },
+    update: {},
+    create: {
+      email: 'admin@gmail.com',
+      name: 'Admin',
+      password: await hash('admin', saltRounds),
+      username: 'Admin',
+      profilePicture: '/profile.webp',
+      isActive: true,
+      role: 'Admin',
     },
   });
 
   // !Debugging Purpose
-  console.log({ testUser });
+  console.log({ testUser, testUser1, testUser2 });
 }
 main()
   .then(async () => {
